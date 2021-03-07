787ad1e13cd4917940fd2af9236b4e3b4e0507e4
ralph.hsu <RaphaHsu@users.noreply.github.com>
2021-03-07 17:42:19 +0800

    test: parse players and dices

--- __tests__/Sibala.test.js
+++ __tests__/Sibala.test.js
@@ -1,9 +1,16 @@
 import game from "../src/Sibala";
-import Sibala from "../封存/src/sibala";
 
 describe('Sibala', function () {
   it('result_SameCategory_ReturnTie', function () {
     const input = 'Amy:6 6 6 6  Lin:6 6 6 6';
     expect(game.result(input)).toBe('Tie.');
+  });
+  it('parseInput_ReturnPlayerNameAndDices', function () {
+    let input = 'Amy:6 6 6 6  Lin:1 1 1 1';
+    const players = game.parseInput(input)
+    expect(players[0].name).toBe('Amy')
+    expect(players[1].name).toBe('Lin')
+    expect(players[0].dices[4][0]).toBe('6')
+    expect(players[1].dices[4][0]).toBe('1')
   });
 });
--- src/Sibala.js
+++ src/Sibala.js
@@ -2,6 +2,18 @@ function result(input) {
   return 'Tie.'
 }
 
+/**
+ * parseInput('Amy:6 6 6 6  Lin:1 1 1 1')
+ *
+ * [
+ *   {name: 'Amy', dices: [6, 6, 6, 6]},
+ *   {name: 'Lin', dices: [1, 1, 1, 1]}
+ * ]
+ */
+function parseInput(input) {
+}
+
 export default {
-  result
+  result,
+  parseInput
 };
