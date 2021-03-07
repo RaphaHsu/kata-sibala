a854041046131071759d7e2c42ffbc21bcdcd98e
ralph.hsu <RaphaHsu@users.noreply.github.com>
2021-03-07 17:00:25 +0800

    project init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sibala
+
+# Description
+
+# Test
---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-class-properties",
+  ],
+  presets: [
+    [ '@babel/preset-env', { targets: { node: 'current' } } ],
+  ],
+};
