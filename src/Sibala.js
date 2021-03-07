621725990ec6fffdcbd96992ae66e3ede73c095c
ralph.hsu <RaphaHsu@users.noreply.github.com>
2021-03-07 17:51:53 +0800

    feat: parse players and dices

--- src/Sibala.js
+++ src/Sibala.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 function result(input) {
   return 'Tie.'
 }
@@ -11,6 +13,21 @@ function result(input) {
  * ]
  */
 function parseInput(input) {
+  const [ firstPlayerInput, secondPlayerInput ] = input.split('  ');
+
+  function getPlayer(text) {
+    const [ name, dicesString ] = text.split(':')
+    const dicesArray = dicesString.split(' ').map(val => parseInt(val));
+    const dices = _.invertBy(_.countBy(dicesArray))
+    return { name, dices }
+  }
+
+  const firstPlayer = getPlayer(firstPlayerInput)
+  const secondPlayer = getPlayer(secondPlayerInput)
+
+  return [
+    firstPlayer, secondPlayer
+  ]
 }
 
 export default {
