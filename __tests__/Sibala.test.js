5ceb2157c2fddc37bddb975dbcb52cf81ead5880
ralph.hsu <RaphaHsu@users.noreply.github.com>
2021-03-07 20:26:25 +0800

    test(Sibala): all the same kind bigger than normal point

--- __tests__/Sibala.test.js
+++ __tests__/Sibala.test.js
@@ -38,6 +38,10 @@ describe('Sibala', function () {
       const input = 'Amy:1 1 1 1  Lin:1 2 3 4'
       expect(game.result(input)).toBe('Amy wins, all the same kind: 1');
     });
+    it('All the same kind bigger than normal point', function () {
+      const input = 'Amy:1 1 1 1  Lin:1 1 2 2'
+      expect(game.result(input)).toBe('Amy wins, all the same kind: 1');
+    });
   });
   it('parseInput_ReturnPlayerNameAndDices', function () {
     let input = 'Amy:6 6 6 6  Lin:1 1 1 1';
