56a0b0f15b53688dfb59135cd08b3bc7cbf286eb
ralph.hsu <RaphaHsu@users.noreply.github.com>
2021-03-07 19:08:56 +0800

    test(Sibala): winning play of same category in situation of all the same kind.

--- __tests__/Sibala.test.js
+++ __tests__/Sibala.test.js
@@ -1,9 +1,20 @@
 import game from "../src/Sibala";
 
 describe('Sibala', function () {
-  it('result_SameCategory_ReturnTie', function () {
-    const input = 'Amy:6 6 6 6  Lin:6 6 6 6';
-    expect(game.result(input)).toBe('Tie.');
+  describe('Same category', function () {
+    describe('All the same kind ', function () {
+      it('result_SameDices_ReturnTie', function () {
+        const input = 'Amy:6 6 6 6  Lin:6 6 6 6';
+        expect(game.result(input)).toBe('Tie.');
+      });
+      it('result_DifferentDices_ReturnWin', function () {
+        const input = 'Amy:6 6 6 6  Lin:1 1 1 1';
+        expect(game.result(input)).toBe('Amy wins, all the same kind: 6');
+      });
+    });
+    it('', function () {
+
+    });
   });
   it('parseInput_ReturnPlayerNameAndDices', function () {
     let input = 'Amy:6 6 6 6  Lin:1 1 1 1';
